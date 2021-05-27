6cdd9643950e4fda17fd701af9dc43930453c489
Denys Vitali <denys.vitali@swisscom.com>
2021-05-27 16:16:34 +0200

    refactor(platform): delete Version variable

--- platform/platform.go
+++ platform/platform.go
@@ -2,7 +2,5 @@ package platform
 
 import "github.com/hashicorp/waypoint-plugin-sdk/component"
 
-var Version = "dev"
-
 var _ component.Platform = (*Platform)(nil)
 var _ component.Generation = (*Platform)(nil)
